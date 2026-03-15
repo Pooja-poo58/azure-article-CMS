5551c59159052a4d0c89921d722e1bb710078e3c
Pooja R <poojashreepj@gmail.com>
2026-03-16 00:40:18 +0530

    added login

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -12,7 +12,6 @@ from flask_login import current_user, login_user, logout_user, login_required
 from FlaskWebProject.models import User, Post
 import msal
 import uuid
-import logging
 
 imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.net/' + app.config['BLOB_CONTAINER']  + '/'
 
@@ -67,10 +66,10 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            logging.warning("Invalid Login attempt")
+            print("Invalid Login attempt")
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        logging.warning("admin logged in successfully")
+        print("admin logged in successfully")
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
