9aa47d5668e37d40ea307fc0dda05fcfcbc96e11
Pooja R <poojashreepj@gmail.com>
2026-03-15 21:54:27 +0530

    remove client secret from code

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ class Config(object):
 
     ### Info for MS Authentication ###
     ### As adapted from: https://github.com/Azure-Samples/ms-identity-python-webapp ###
-    CLIENT_SECRET = "Axs8Q~1IOEC-rz5kRSqOYKdVISG6EMdWGr0ZYbs0"
+    CLIENT_SECRET = os.getenv("CLIENT_SECRET")
     # In your production app, Microsoft recommends you to use other ways to store your secret,
     # such as KeyVault, or environment variable as described in Flask's documentation here:
     # https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
