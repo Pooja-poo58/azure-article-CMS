e84a1cc373e6eb2f4217712d8fc49817ff35a40c
Pooja R <poojashreepj@gmail.com>
2026-03-16 20:32:17 +0530

    added login logs

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -71,6 +71,7 @@ def login():
             return redirect(url_for('login'))
         print("admin logged in successfully")
         login_user(user, remember=form.remember_me.data)
+        print("Successfuly logged in ")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
